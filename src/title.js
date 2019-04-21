7ba68f687fd01c636bbc984b5858d6338b0c4692
alias <saika@toki.waseda.jp>
2019-04-21 13:12:24 +0900

    feat: title component

---
+++ src/title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render () {
+    return (
+      <h1>This is title component</h1>
+    )
+  }
+}
+
+export default Title;
