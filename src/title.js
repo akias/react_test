3402e08b588d1a09898708d9308ad203b6d23fd8
alias <saika@toki.waseda.jp>
2019-04-21 13:40:05 +0900

    feat: event listener parameter

--- src/title.js
+++ src/title.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 class Title extends Component {
-  handleClickOnTitle(){
-    console.log('Click on title.')
+  handleClickOnTitle(str){
+    alert(`${str} click on title.`)
   }
   render () {
     return (
-      <h1 onClick={this.handleClickOnTitle}>This is title component</h1>
+      <h1 onClick={this.handleClickOnTitle.bind(this, 'Do not')}>This is title component</h1>
     )
   }
 }
